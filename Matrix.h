23bd10057117856c678620152cd975f7cf076596
Izhoy <108609622+Izhoy@users.noreply.github.com>
2022-09-13 22:21:49 +0300

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -14,6 +14,7 @@ string err_invalid_arguments_minor = "Input arguments are not acceptable (Minor 
 string err_invalid_arguments_index = "Input argumants are not acceptable (operator () )";
 string err_determinant_is_zero = "Determinant of matrix is equal to zero (Inverse function)";
 string err_arguments_set_element = "Arguments are greater than matrix dimensions (setElement function)";
+string err_squeeze = "Input Arguments are greater than matrix dimensions (squeeze function)";
 
 template <typename T = int>
 class Matrix
@@ -25,8 +26,8 @@ private:
 
 public:
 	Matrix(size_t RowCount = 1, size_t ColumnCount = 1);  // Constructor
-	Matrix(const Matrix&); // Copy constructor
-	Matrix(Matrix&&);  // Move constructor
+	Matrix(const Matrix<T>&); // Copy constructor
+	Matrix(Matrix<T>&&);  // Move constructor
 	~Matrix();  // Destructor
 
 	const size_t getRowCount() { return RowCount; }  // Get methods
@@ -39,13 +40,15 @@ public:
 	Matrix<T> Minor(unsigned int, unsigned int);  // Minor of matrix
 	const T Determinant(); // Determinant of matrix
 	Matrix<double> Inverse(); // Calculating inverse matrix
+	static void to_square_size(Matrix<T>&, Matrix<T>&); // Addition to square matrices нулями
+	Matrix<T> squeeze(size_t, size_t); // Compress to the desired size
 
 	//overload of operators
-	Matrix operator=(const Matrix&); // Assignment
-	Matrix operator=(Matrix&&);  // Assignment with moving
-	Matrix operator+(const Matrix&); // Addition
-	Matrix operator-(const Matrix&); // Substraction
-	Matrix operator*(const Matrix&); // Multiplication
+	Matrix<T> operator=(const Matrix<T>&); // Assignment
+	Matrix<T> operator=(Matrix<T>&&);  // Assignment with moving
+	Matrix<T> operator+(const Matrix<T>&); // Addition
+	Matrix<T> operator-(const Matrix<T>&); // Substraction
+	Matrix<T> operator*(const Matrix<T>&); // Multiplication
 	const T &operator()(unsigned int, unsigned int); // Indexing
 
 	template <typename T>
@@ -87,7 +90,7 @@ Matrix<T>::Matrix(size_t RowCount, size_t ColumnCount) {
 
 // Copy constructor
 template <typename T>
-Matrix<T>::Matrix(const Matrix& A)
+Matrix<T>::Matrix(const Matrix<T>& A)
 	:RowCount(A.RowCount), ColumnCount(A.ColumnCount) {
 
 	size_t i = 0;
@@ -116,7 +119,7 @@ Matrix<T>::Matrix(const Matrix& A)
 
 // Move constructor
 template <typename T>
-Matrix<T>::Matrix(Matrix&& A) :
+Matrix<T>::Matrix(Matrix<T>&& A) :
 	RowCount(A.RowCount), ColumnCount(A.ColumnCount), matrix(A.matrix) {
 	A.RowCount = 0;
 	A.ColumnCount = 0;
@@ -162,7 +165,7 @@ void Matrix<T>::fillRandom(int min, int max) {
 // Transposition of matrix
 template <typename T>
 const Matrix<T> Matrix<T>::Transposition() {
-	Matrix<T> Tran = Matrix(ColumnCount, RowCount);
+	Matrix<T> Tran = Matrix<T>(ColumnCount, RowCount);
 
 	for (size_t i = 0; i < RowCount; ++i) {
 		for (size_t j = 0; j < ColumnCount; ++j) {
@@ -228,9 +231,46 @@ Matrix<double> Matrix<T>::Inverse(){
 	return Inv.Transposition();
 }
 
+template<typename T>
+void Matrix<T>::to_square_size(Matrix<T>& A, Matrix<T>& B){
+	size_t n = max(max(A.RowCount, A.ColumnCount), max(B.RowCount, B.ColumnCount));
+	if (n % 2 != 0) ++n;
+
+	Matrix<T> Atemp(n, n), Btemp(n, n);
+	for (size_t i = 0; i < n; ++i) {
+		for (size_t j = 0; j < n; ++j) {
+			if (i > A.RowCount - 1 || j > A.ColumnCount - 1)
+				Atemp.matrix[i][j] = 0;
+			else
+				Atemp.matrix[i][j] = A.matrix[i][j];
+			if (i > B.RowCount - 1 || j > B.ColumnCount - 1)
+				Btemp.matrix[i][j] = 0;
+			else
+				Btemp.matrix[i][j] = B.matrix[i][j];
+		}
+	}
+	A = Atemp;
+	B = Btemp;
+}
+
+template<typename T>
+Matrix<T> Matrix<T>::squeeze(size_t x, size_t y){
+	if (x > this->RowCount || y > this->ColumnCount)
+		throw invalid_argument(err_squeeze);
+
+	Matrix<T> Temp(x, y);
+	for (size_t i = 0; i < x; ++i) {
+		for (size_t j = 0; j < y; ++j) {
+			Temp.matrix[i][j] = this->matrix[i][j];
+		}
+	}
+
+	return Temp;
+}
+
 // Overload of assignment operator
 template <typename T>
-Matrix<T> Matrix<T>::operator=(const Matrix& A) {
+Matrix<T> Matrix<T>::operator=(const Matrix<T>& A) {
 	if (matrix != nullptr) {
 		for (size_t i = 0; i < this->RowCount; ++i) {
 			if (matrix[i] != nullptr)
@@ -270,7 +310,7 @@ Matrix<T> Matrix<T>::operator=(const Matrix& A) {
 
 // Overload of assignment operator with moving
 template <typename T>
-Matrix<T> Matrix<T>::operator=(Matrix&& A) {
+Matrix<T> Matrix<T>::operator=(Matrix<T>&& A) {
 	if (matrix != nullptr) {
 		for (size_t i = 0; i < this->RowCount; ++i) {
 			if (matrix[i] != nullptr)
@@ -292,7 +332,7 @@ Matrix<T> Matrix<T>::operator=(Matrix&& A) {
 
 // Overload of addition operator
 template <typename T>
-Matrix<T> Matrix<T>::operator+(const Matrix& A) {
+Matrix<T> Matrix<T>::operator+(const Matrix<T>& A) {
 
 	if (RowCount != A.RowCount || ColumnCount != A.ColumnCount) {
 		throw invalid_argument(err_dimension);
@@ -311,7 +351,7 @@ Matrix<T> Matrix<T>::operator+(const Matrix& A) {
 
 // Overload of substraction operator
 template <typename T>
-Matrix<T> Matrix<T>::operator-(const Matrix& A) {
+Matrix<T> Matrix<T>::operator-(const Matrix<T>& A) {
 	if (RowCount != A.RowCount || ColumnCount != A.ColumnCount) {
 		throw invalid_argument(err_dimension);
 	}
@@ -329,23 +369,79 @@ Matrix<T> Matrix<T>::operator-(const Matrix& A) {
 
 // Overload of multiplication operator
 template <typename T>
-Matrix<T> Matrix<T>::operator*(const Matrix& A) {
-	if (RowCount != A.ColumnCount || ColumnCount != A.RowCount) {
+Matrix<T> Matrix<T>::operator*(const Matrix<T>& A) {
+	if (this->RowCount != Right.ColumnCount || this->ColumnCount != Right.RowCount)
 		throw invalid_argument(err_dimension);
+
+	// Base case
+	if (this->ColumnCount == 1 && this->RowCount == 1 &&
+		Right.ColumnCount == 1 && Right.RowCount == 1) {
+		Matrix<T> XY(1, 1);
+		XY.matrix[0][0] = this->matrix[0][0] * Right.matrix[0][0];
+		return XY;
 	}
 
-	Matrix<T> Product = Matrix<T>(RowCount, A.ColumnCount);
-
-	for (size_t i = 0; i < Product.RowCount; ++i) {
-		for (size_t j = 0; j < Product.ColumnCount; ++j) {
-			Product.matrix[i][j] = 0;
-			for (size_t k = 0; k < ColumnCount; ++k) {
-				Product.matrix[i][j] += matrix[i][k] * A.matrix[k][j];
-			}
+	// We complement the matrices to even square matrices
+	Matrix<T> X = *this, Y = Right;
+	to_square_size(X, Y);
+	size_t n = X.RowCount; // Size of matrices
+	// Required variables
+	Matrix<T> A(n / 2, n / 2), B(n / 2, n / 2), C(n / 2, n / 2), D(n / 2, n / 2),
+		E(n / 2, n / 2), F(n / 2, n / 2), G(n / 2, n / 2), H(n / 2, n / 2),
+		P1(n / 2, n / 2), P2(n / 2, n / 2), P3(n / 2, n / 2), P4(n / 2, n / 2),
+		P5(n / 2, n / 2), P6(n / 2, n / 2), P7(n / 2, n / 2);
+	
+	for (size_t i = 0; i < n / 2; ++i) {
+		for (size_t j = 0; j < n / 2; ++j) {
+			A.matrix[i][j] = X.matrix[i][j];
+			B.matrix[i][j] = X.matrix[i][j + n/2];
+			C.matrix[i][j] = X.matrix[i + n/2][j];
+			D.matrix[i][j] = X.matrix[i + n/2][j + n/2];
+			E.matrix[i][j] = Y.matrix[i][j];
+			F.matrix[i][j] = Y.matrix[i][j + n/2];
+			G.matrix[i][j] = Y.matrix[i + n/2][j];
+			H.matrix[i][j] = Y.matrix[i + n/2][j + n/2];
 		}
 	}
+	// Recursively multiply
+	P1 = A * (F - H);
+	P2 = (A + B) * H;
+	P3 = (C + D) * E;
+	P4 = D * (G - E);
+	P5 = (A + D) * (E + H);
+	P6 = (B - D) * (G + H);
+	P7 = (A - C) * (E + F);
 
-	return Product;
+	Matrix<T> XY11(n / 2, n / 2), XY12(n / 2, n / 2), XY21(n / 2, n / 2), XY22(n / 2, n / 2);
+
+	XY11 = P5 + P4 - P2 + P6;
+	XY12 = P1 + P2;
+	XY21 = P3 + P4;
+	XY22 = P1 + P5 - P3 - P7;
+	Matrix<T> XY(n, n);
+	// Filling in the final matrix
+	for (size_t i = 0; i < n/2; ++i) {
+		for (size_t j = 0; j < n/2; ++j) {
+			XY.matrix[i][j] = XY11.matrix[i][j];
+		}
+	}
+	for (size_t i = 0; i < n/2; ++i) {
+		for (size_t j = n/2; j < n; ++j) {
+			XY.matrix[i][j] = XY12.matrix[i][j - n/2];
+		}
+	}
+	for (size_t i = n/2; i < n; ++i) {
+		for (size_t j = 0; j < n/2; ++j) {
+			XY.matrix[i][j] = XY21.matrix[i - n/2][j];
+		}
+	}
+	for (size_t i = n/2; i < n; ++i) {
+		for (size_t j = n/2; j < n; ++j) {
+			XY.matrix[i][j] = XY22.matrix[i-n/2][j-n/2];
+		}
+	}
+	// Deleting null rows and columns
+	return XY.squeeze(this->RowCount, Right.ColumnCount);
 }
 
 // Overload of indexing
